414dad0f8a83135f11fae5e5ed672b38f42eb218
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-03-22 11:14:23 +0100

    Update action_push.c

--- src/action_push.c
+++ src/action_push.c
@@ -15,18 +15,33 @@
 // *top is the pointer to the top element of *stack.
 // ft_pa(): Take the first element at the top of 'b' and put it 
 // at the top of 'a'. Do nothing if b is empty
-void pa(int *stack_a, int *top_a, int *stack_b, int *top_b)
+void pb(t_data *data)
 {
-  if (*top_b >= 0)
-    stack_a[++(*top_a)] = stack_b[(*top_b)--];
-  ft_printf("pa\n");  
+	t_list *top;
+	
+    if (data->stack_a)
+	{
+		top = data->stack_a;
+		data->stack_a = top->next;
+		top->next = data->stack_b;
+		data->stack_b = top;
+		printf("pb\n");
+	}
 }
 
 // ft_pb(): Take the first element at the top of 'a' and put it 
 // at the top of 'b'. Do nothing if a is empty.
-void pb(int *stack_a, int *top_a, int *stack_b, int *top_b)
+void pa(t_data *data) ///DONE
 {
-  if (*top_a >= 0)
-    stack_b[++(*top_b)] = stack_a[(*top_a)--];
-  ft_printf("pb\n"); 
+	t_list *top;
+	
+    if (data->stack_b)
+	{
+		top = data->stack_b;
+		data->stack_b = top->next;
+		top->next = data->stack_a;
+		data->stack_a = top;
+		printf("pa\n");
+	}
 }
+
