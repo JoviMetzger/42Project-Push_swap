0ef1a5735a3a3983a4176c3b4e3bea37ae6081e6
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-03 16:22:19 +0200

    Update action_reverse.c

--- src/action_reverse.c
+++ src/action_reverse.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   action_reverse.c                                :+:    :+:            */
+/*   action_reverse.c                                   :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/02/16 11:54:39 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/02/16 11:57:39 by jmetzger      ########   odam.nl         */
+/*   Created: 2023/04/03 14:54:57 by jmetzger      #+#    #+#                 */
+/*   Updated: 2023/04/03 14:55:03 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// *top is the pointer to the top element of *stack.
-// ft_rra(): Shift down all elements of stack_a by 1.
+// action_reverse_a() & rra(): Shift down all elements of stack_a by 1.
 // The last element becomes the first one.
-void rra(t_data *data)
+static void action_reverse_a(t_data *data)
 {
-	t_list *prev_last;
-	t_list *last;
+	t_stack *prev_last;
+	t_stack *last;
 	
     if (data->stack_a)
 	{
@@ -33,15 +32,14 @@ void rra(t_data *data)
 		last->next = data->stack_a;
 		data->stack_a = last;	
 	}
-	printf("rra\n");
 }
 
-// ft_rrb(): Shift down all elements of stack_b by 1.
+// action_reverse_b() & rrb(): Shift down all elements of stack_b by 1.
 // The last element becomes the first one.
-void rrb(t_data *data)
+static void action_reverse_b(t_data *data)
 {
-	t_list *prev_last;
-	t_list *last;
+	t_stack *prev_last;
+	t_stack *last;
 	
     if (data->stack_b)
 	{
@@ -56,13 +54,24 @@ void rrb(t_data *data)
 		last->next = data->stack_b;
 		data->stack_b = last;	
 	}
+}
+
+void rra(t_data *data)
+{
+   	action_reverse_a(data);
+	printf("rra\n");
+}
+
+void rrb(t_data *data)
+{
+   	action_reverse_b(data);
 	printf("rrb\n");
 }
 
-// ft_rrr(): rra and rrb at the same time.
+// rrr(): rra and rrb at the same time.
 void rrr(t_data *data)
 {
-    rra(data);
-    rrb(data);
+    action_reverse_a(data);
+    action_reverse_b(data);
 	printf("rrr\n");
 }
