ee00e77ceb2a8ee7e34b77436411fbdc9df80eb7
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-03 16:30:53 +0200

    Update swap_utils.c

--- src/swap_utils.c
+++ src/swap_utils.c
@@ -6,12 +6,13 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/05 15:16:42 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/03/06 10:55:53 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/03 13:03:30 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+//
 void ft_error(char *msg)
 {
     ft_putstr(msg);
@@ -19,6 +20,19 @@ void ft_error(char *msg)
     exit(0); 
 }
 
+//
+int stack_sorted(t_stack *stack)
+{
+	while (stack && stack->next)
+	{
+		if (stack->content > stack->next->content)
+			return(0);
+		stack = stack->next;
+	}
+	return(1);
+}
+
+//
 void    ft_free(char **str)
 {
 	unsigned int	i;
@@ -30,4 +44,49 @@ void    ft_free(char **str)
 		i++;
 	}
 	free(str);
+}
+
+//
+int ft_min(t_stack *stack)
+{
+	int smallest;
+
+	smallest = stack->content;
+	while (stack)
+	{
+		if (stack->content < smallest)
+			smallest = stack->content; 
+		stack = stack->next;
+	}
+	return (smallest);
+}
+
+//
+int ft_max(t_stack *stack)
+{
+	int biggest;
+	
+	biggest = stack->content;
+	while (stack)
+	{
+		if (stack->content > biggest)
+			biggest = stack->content;
+		stack = stack->next;
+	}
+	return (biggest);
+}
+
+//
+int	find_index(t_stack *stack, int nbr)
+{
+	int		i;
+
+	i = 0;
+	while (stack->content != nbr)
+	{
+		i++;
+		stack = stack->next;
+	}
+	stack->index = 0;
+	return (i);
 }
