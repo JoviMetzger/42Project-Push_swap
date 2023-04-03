88a4b3c08ab78af766b1d0c320a1e937fd1e7108
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-03 16:23:09 +0200

    Update action_swap.c

--- src/action_swap.c
+++ src/action_swap.c
@@ -1,57 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   action_swap.c                                   :+:    :+:            */
+/*   action_swap.c                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/02/16 11:53:45 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/02/16 11:57:03 by jmetzger      ########   odam.nl         */
+/*   Created: 2023/04/03 14:56:02 by jmetzger      #+#    #+#                 */
+/*   Updated: 2023/04/03 14:56:18 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 // *top is the pointer to the top element of *stack.
-// ft_sa(): Swap the first 2 elements at the top of stack_a. 
-// Do nothing if there is only one or no elements.
+// action_swap(): Swap the first 2 elements at the top of stack. 
+static void action_swap(t_stack *stack) 
+{
+	int tmp;
+	
+    if (stack) 
+	{
+		tmp = stack->content;
+		stack->content = stack->next->content;
+		stack->next->content = tmp;
+	}
+}
+
+// sa() swaps first 2 elements in stack_a.
 void sa(t_data *data)
 {
-	int a;
-	int b;
-	
-    if (data->stack_a) 
-	{
-		a = data->stack_a->content;
-		b = data->stack_a->next->content;
-		data->stack_a->content = b;
-		data->stack_a->next->content = a;
-	}
+   	action_swap(data->stack_a);
 	printf("sa\n");
 }
 
-// ft_sb(): Swap the first 2 elements at the top of stack_b.
-// Do nothing if there is only one or no elements.
-void sb(t_data *data) 
+// sb() swaps first 2 elements in stack_b.
+void sb(t_data *data)
 {
-	int a;
-	int b;
-	
-    if (data->stack_b) 
-	{
-		b = data->stack_b->content;
-		a = data->stack_b->next->content;
-		data->stack_b->content = a;
-		data->stack_b->next->content = b;
-	}
+   	action_swap(data->stack_b);
 	printf("sb\n");
 }
 
-// ft_ss(): sa and sb at the same time.
+// ss(): sa and sb at the same time.
 void ss(t_data *data) 
 {
-    sa(data);
-    sb(data);
+    action_swap(data->stack_a);
+    action_swap(data->stack_b);
 	printf("ss\n");
 }
-
