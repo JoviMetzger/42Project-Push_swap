d89b063dd7f0f68be24543c76851be42debd0d83
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-05 22:59:35 +0200

    Update sorting_A.c

--- src/sorting_A.c
+++ src/sorting_A.c
@@ -13,8 +13,6 @@
 #include "push_swap.h"
 
 //
-
-//
 void sort_3(t_data *data)
 {  
   if (ft_min(data->stack_a) == data->stack_a->content)
@@ -36,78 +34,7 @@ void sort_3(t_data *data)
       sa(data);
   }
 }
-//-----------------------------------------------------------
 
-// int findPivot(t_stack *stack, int size) {
-//     int i = 0;
-//     int middle = size / 2; 
-
-//     while (i < middle) { 
-//         int j = 0;
-//         while (j < size - i - 1) {
-//             if (stack->content > stack->next->content) {
-//                 int temp = stack->content;
-//                 stack->content = stack->next->content;
-//                 stack->next->content = temp;
-//             }
-//             j++;
-//         }
-//         i++;
-//     }
-
-//     int pivot = stack->content;
-//     return (pivot);
-// }
-
-// void pivot_point(t_data *data, int pivot) 
-// {
-// 	int start;
-	
-// 	start = (int)data->stack_a->content;
-//   if (start = pivot) 
-// 	{
-//     pb(data);
-// 		pivot_point(data, pivot);
-// 	}
-//   if (start > pivot)
-// 	{
-// 		ra(data);
-// 		pivot_point(data, pivot);
-// 	}
-// 	// if (start == pivot)
-// 	// {
-// 	// 	print("ni");
-// 	// }
-// }
-//----------------------------------------------------
-
-int findMinCommands(int stackA[], int stackB[], int sizeA, int sizeB) {
-    int minCommands = -1; // Initialize with a large value to track minimum commands
-    int targetNum; // Number to be moved from B to A
-    int numCommands; // Number of commands needed to move targetNum to its appropriate position in A
-
-    // Loop through each number in B stack
-    for (int i = 0; i < sizeB; i++) {
-        targetNum = stackB[i]; // Number to be moved from B to A
-
-        // Find the position of targetNum in A stack
-        for (int j = 0; j < sizeA; j++) {
-            if (stackA[j] > targetNum) {
-                numCommands = i + j + 1; // Number of commands needed to move targetNum to its appropriate position in A
-                break;
-            }
-        }
-
-        // Update minCommands if the current number of commands is smaller than the previously tracked minimum
-        if (minCommands == -1 || numCommands < minCommands) {
-            minCommands = numCommands;
-        }
-    }
-
-    return minCommands;
-}
-
-//----------------------------------------------------
 //
 void sorting_stack_A(t_data *data)
 {
