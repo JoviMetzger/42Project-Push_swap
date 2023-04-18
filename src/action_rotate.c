38621835b76b6b880f1041abd92661203e9e0feb
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-18 06:12:03 +0200

    Update action_rotate.c

--- src/action_rotate.c
+++ src/action_rotate.c
@@ -12,8 +12,10 @@
 
 #include "push_swap.h"
 
-// action_rotate(): Shift up all elements of stack by 1.
-// The first element becomes the last one.
+/*	action_rotate(): 
+*		Shift up all elements of the stack by 1.
+*		The first element becomes the last one.
+*/
 static void action_rotate(t_stack **stack)
 {
    t_stack	*tmp;
@@ -28,22 +30,31 @@ static void action_rotate(t_stack **stack)
 	tmp->next->next = NULL;
 }
 
+/*	ra(): 
+*		Rotates the elements up in stack_a.
+*/
 void ra(t_stack **stack_a)
 {
    	action_rotate(stack_a);
 	ft_putstr("ra\n");
 }
 
+/*	rb(): 
+*		Rotates the elements up in stack_b.
+*/
 void rb(t_stack **stack_b)
 {
    	action_rotate(stack_b);
 	ft_putstr("rb\n");
 }
 
-// rr(): ra and rb at the same time.
+/*	rr(): 
+*		Does the rotateing in stack_a and stack_b at the same time.
+*/
 void rr(t_stack **stack_a, t_stack **stack_b) 
 {
     action_rotate(stack_a);
     action_rotate(stack_b);
 	ft_putstr("rr\n");
 }
+
