6962224df09a47e0909c892213a56b1c4b43f2c3
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-20 19:41:53 +0200

    Update checks.c

--- src/checks.c
+++ src/checks.c
@@ -6,66 +6,74 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 11:46:25 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/04/03 16:59:18 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/20 18:40:42 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-//
-static void ft_is_num(char *num)
+/* ft_is_num(): 
+*	Checks if the input contains any non-digit characters. 
+*	If it does, the function exits the program with an error message.
+*/
+static void	ft_is_num(char *num)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
 	if (num[0] == '-')
 		i++;
 	while (num[i])
 	{
 		if (!ft_isdigit(num[i]))
-			ft_error("Error, please check if your input only consists of digit's");
+			ft_error("Error, only digit's are allowed");
 		i++;
 	}
 }
 
-//
-static void ft_double_num(char **num)
+/* ft_douple_num(): 
+*	Checks for duplicate numbers in the input array 
+*	by comparing them using the strcmp() function. 
+*	If any duplicates are found, the function exits 
+*	the program and displays an error message.
+*/
+static void	ft_double_num(char **num)
 {
-	int i;
-	int j;
-	int comp;
-	
+	int	i;
+	int	j;
+	int	comp;
+
 	i = 0;
-    while (num[i]) 
+	while (num[i])
 	{
 		j = i + 1;
-        while (num[j]) 
+		while (num[j])
 		{
 			comp = ft_strcmp(num[i], num[j]);
 			if (comp == 0)
 				ft_error("Error, some arguments are duplicates");
 			j++;
-        }
+		}
 		i++;
-    }
+	}
 }
 
-//
-void ft_minus_zero_check(char *num)
+/* checking_arg(): 
+*	Converts a string input into an array and checks if the 
+*	given input is valid.
+*		- It first converts the input string into an array using 
+*		  the ft_split() function.
+*		- Then, it converts each character in the array into an 
+*		  integer using the ft_atoi() function.
+*		- After that, it checks if the input contains only numerical values, 
+*		  and whether there are any duplicates. 
+*		  Additionally, it handles specific cases such as the presence 
+*		  of negative zero ("-0") values.
+*/
+void	checking_arg(int argc, char **argv)
 {
-    int i;
-	
-	i = 0;
-	if (num[0] == '-' && num[1] == '0')
-        ft_error("Error, use of '-0' doesn't work");
-	i++;
-}
-
-//
-void checking_arg(int argc, char **argv)
-{
-	int i;
-	char **array;
+	int		i;
+	char	**array;
 
 	i = 0;
 	if (argc == 2)
@@ -79,7 +87,6 @@ void checking_arg(int argc, char **argv)
 	{
 		ft_atoi(array[i]);
 		ft_is_num(array[i]);
-		ft_minus_zero_check(array[i]);
 		ft_double_num(array);
 		i++;
 	}
