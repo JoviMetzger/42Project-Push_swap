10a231c2b9fa1bddfe4008fa2e6304b629fc11a5
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-20 19:45:36 +0200

    Update t_list.c

--- src/t_list.c
+++ src/t_list.c
@@ -6,32 +6,20 @@
 /*   By: jmetzger <jmetzger@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/20 22:28:19 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/04/03 13:03:09 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/20 19:26:09 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// 
-int	ft_lstsize_a(t_stack *lst)
-{
-	int	i;
-
-	i = 0;
-	while (lst)
-	{
-		lst = lst->next;
-		i++;
-	}
-	return (i);
-}
-
-//
-t_stack	*ft_lstlast_a(t_stack *head)
+/* ft_lstlast_a(): 
+*	Returns the last element/node of the stack.
+*/
+t_stack	*ft_lstlast_a(t_stack *stack)
 {
 	t_stack	*tmp;
 
-	tmp = head;
+	tmp = stack;
 	while (tmp->next)
 	{
 		tmp = tmp->next;
@@ -41,7 +29,31 @@ t_stack	*ft_lstlast_a(t_stack *head)
 	return (tmp);
 }
 
-//
+/* ft_lstnew_a(): 
+*	Creates a new element/node inside a stack with the provided value.
+*	NOTE: for push_swap I needed to set index, pos, furure_pos, 
+*	      cost_a, cost_b for this given value to '-1'
+*/
+t_stack	*ft_lstnew_a(int value)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof * new);
+	if (!new)
+		return (NULL);
+	new->content = value;
+	new->index = -1;
+	new->pos = -1;
+	new->future_pos = -1;
+	new->cost_a = -1;
+	new->cost_b = -1;
+	new->next = NULL;
+	return (new);
+}
+
+/* ft_lstadd_back_a(): 
+*	Adds an element/node to the end of a stack.
+*/
 void	ft_lstadd_back_a(t_stack **stack, t_stack *new)
 {
 	t_stack	*n;
@@ -59,16 +71,18 @@ void	ft_lstadd_back_a(t_stack **stack, t_stack *new)
 	}
 }
 
-//
-t_stack	*ft_lstnew_a(int value)
+/* ft_lstsize_a(): 
+*	Returns the number of elements/nodes in the stack.
+*/
+int	ft_lstsize_a(t_stack *lst)
 {
-	t_stack	*new;
+	int	i;
 
-	new = (t_stack *) malloc(sizeof(*new));
-	if (!new)
-		return (NULL);
-	new->content = value;
-	new->index = -1;
-	new->next = NULL;
-	return (new);
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
 }
