444f1d38266a438aa778ace36a72d6e319878aea
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-20 19:40:56 +0200

    Update action_swap.c

--- src/action_swap.c
+++ src/action_swap.c
@@ -6,20 +6,21 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 14:56:02 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/04/03 14:56:18 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/20 18:27:22 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// *top is the pointer to the top element of *stack.
-// action_swap(): Swap the first 2 elements at the top of stack. 
-// NOTE: very important to swap the index aswell, otherwise swap won't work.
-static void action_swap(t_stack *stack) 
+/* action_swap(): 
+*	Swap the first 2 elements at the top of the giving stack. 
+*	NOTE: very important to swap the index aswell, otherwise swap won't work.
+*/
+static void	action_swap(t_stack *stack)
 {
-	int tmp;
-	
-    if (stack) 
+	int	tmp;
+
+	if (stack)
 	{
 		tmp = stack->content;
 		stack->content = stack->next->content;
@@ -30,25 +31,30 @@ static void action_swap(t_stack *stack)
 	}
 }
 
-// sa() swaps first 2 elements in stack_a.
-void sa(t_stack **stack_a)
+/* sa(): 
+*	Swaps first 2 elements in stack_a.
+*/
+void	sa(t_stack **stack_a)
 {
-   	action_swap(*stack_a);
+	action_swap(*stack_a);
 	ft_putstr("sa\n");
 }
 
-// sb() swaps first 2 elements in stack_b.
-void sb(t_stack **stack_b)
+/* sb(): 
+*	Swaps first 2 elements in stack_b.
+*/
+void	sb(t_stack **stack_b)
 {
-   	action_swap(*stack_b);
+	action_swap(*stack_b);
 	ft_putstr("sb\n");
 }
 
-// ss(): sa and sb at the same time.
-void ss(t_stack **stack_a, t_stack **stack_b) 
+/* ss(): 
+*	Does the swap in stack_a and stack_b at the same time.
+*/
+void	ss(t_stack **stack_a, t_stack **stack_b)
 {
-    action_swap(*stack_a);
-    action_swap(*stack_b);
+	action_swap(*stack_a);
+	action_swap(*stack_b);
 	ft_putstr("ss\n");
 }
-
