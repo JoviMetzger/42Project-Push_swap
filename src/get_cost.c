a18de53cecce77dca5a9683154979d8d7608a151
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-20 19:51:27 +0200

    Update get_cost.c

--- src/get_cost.c
+++ src/get_cost.c
@@ -12,28 +12,28 @@
 
 #include "push_swap.h"
 
-/*	get_cost():
-*		Calculate the cost of moving each element from stack_b to 
-*		stack_a during the sorting process.
-*		The cost is based on the position of the element in the stack. 
-*		For elements in stack_b, the cost is (size_b - cpy_B->pos) * -1.
-*		For elements in stack_a, the cost is (size_a - cpy_B->future_pos) * -1.
-*		The size of the cost indicates how far the element is from 
-*		its sorted position.
-*		If the element is closer to the bottom of the stack, the cost is lower. 
-*		If the element is closer to the top of the stack, the cost is higher.
-*			- Examle:	
-*					size_b = 10;
-*					pos = 3;
-*					(10 - 3) * -1 = -7;
-*			- This negative number (-7) indicates that the cost of moving 
-*			  this element to stack_a is low. 
-*			- Example:	
+/* get_cost():
+*	Calculate the cost of moving each element from stack_b to 
+*	stack_a during the sorting process.
+*	The cost is based on the position of the element in the stack. 
+*	For elements in stack_b, the cost is (size_b - cpy_B->pos) * -1.
+*	For elements in stack_a, the cost is (size_a - cpy_B->future_pos) * -1.
+*	The size of the cost indicates how far the element is from 
+*	its sorted position.
+*	If the element is closer to the bottom of the stack, the cost is lower. 
+*	If the element is closer to the top of the stack, the cost is higher.
+*		- Examle:	
 *				size_b = 10;
-*				pos = 8;
-*				(10 - 8) * -1 = -2;
-*			- This negative number (-2) indicates that the cost of moving 
-*			  this element to stack_a is high.
+*				pos = 3;
+*				(10 - 3) * -1 = -7;
+*		- This negative number (-7) indicates that the cost of moving 
+*		  this element to stack_a is low. 
+*		- Example:	
+*			size_b = 10;
+*			pos = 8;
+*			(10 - 8) * -1 = -2;
+*		- This negative number (-2) indicates that the cost of moving 
+*		  this element to stack_a is high.
 */
 void	get_cost(t_stack **stack_a, t_stack **stack_b)
 {
