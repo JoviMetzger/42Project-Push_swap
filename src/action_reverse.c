5248d367c9fd9d19f81b8be38daf7435f95c04fe
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-20 19:39:28 +0200

    Update action_reverse.c

--- src/action_reverse.c
+++ src/action_reverse.c
@@ -6,17 +6,17 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 14:54:57 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/04/03 14:55:03 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/20 18:22:15 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*	action_reverse(): 
-*		Shift down all elements of the stack by 1.
-*		The last element becomes the first one.
+/* action_reverse(): 
+*	Shift down all elements of the stack by 1.
+*	The last element becomes the first one.
 */
-static void action_reverse(t_stack **stack)
+static void	action_reverse(t_stack **stack)
 {
 	t_stack	*prev;
 	t_stack	*new;
@@ -31,33 +31,33 @@ static void action_reverse(t_stack **stack)
 	}
 	prev->next = *stack;
 	new->next = NULL;
-	*stack = prev;	
+	*stack = prev;
 }
 
-/*	rra(): 
-*		Rotates the elements down in stack_a.
+/* rra(): 
+*	Rotates the elements down in stack_a.
 */
-void rra(t_stack **stack_a)
+void	rra(t_stack **stack_a)
 {
-   	action_reverse(stack_a);
+	action_reverse(stack_a);
 	ft_putstr("rra\n");
 }
 
-/*	rrb(): 
-*		Rotates the elements down in stack_b.
+/* rrb(): 
+*	Rotates the elements down in stack_b.
 */
-void rrb(t_stack **stack_b)
+void	rrb(t_stack **stack_b)
 {
-   	action_reverse(stack_b);
+	action_reverse(stack_b);
 	ft_putstr("rrb\n");
 }
 
-/*	rrr(): 
-*		Does the reverse-rotation in stack_a and stack_b at the same time.
+/* rrr(): 
+*	Does the reverse-rotation in stack_a and stack_b at the same time.
 */
-void rrr(t_stack **stack_a, t_stack **stack_b)
+void	rrr(t_stack **stack_a, t_stack **stack_b)
 {
-    action_reverse(stack_a);
-    action_reverse(stack_b);
+	action_reverse(stack_a);
+	action_reverse(stack_b);
 	ft_putstr("rrr\n");
 }
