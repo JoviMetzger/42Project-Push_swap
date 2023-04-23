79da15d8a7c71b86a6e037fa364914075b2b13b7
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-23 19:40:21 +0200

    Update checks.c

--- src/checks.c
+++ src/checks.c
@@ -6,7 +6,7 @@
 /*   By: jmetzger <jmetzger@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 11:46:25 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/04/23 14:30:31 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/23 18:08:50 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,39 +21,12 @@ static void	ft_is_num(char *num)
 	int	i;
 
 	i = 0;
-	if (num[0] == '-')
+	if (num[0] == '-' || num[0] == '+')
 		i++;
 	while (num[i])
 	{
 		if (!ft_isdigit(num[i]))
 			ft_error("Error");
-		i++;
-	}
-}
-
-/* ft_douple_num(): 
-*	Checks for duplicate numbers in the input array 
-*	by comparing them using the strcmp() function. 
-*	If any duplicates are found, the function exits 
-*	the program and displays an error message.
-*/
-static void	ft_double_num(char **num)
-{
-	int	i;
-	int	j;
-	int	comp;
-
-	i = 0;
-	while (num[i])
-	{
-		j = i + 1;
-		while (num[j])
-		{
-			comp = ft_strcmp(num[i], num[j]);
-			if (comp == 0)
-				ft_error("Error");
-			j++;
-		}
 		i++;
 	}
 }
@@ -65,10 +38,7 @@ static void	ft_double_num(char **num)
 *		  the ft_split() function.
 *		- Then, it converts each character in the array into an 
 *		  integer using the ft_atoi() function.
-*		- After that, it checks if the input contains only numerical values, 
-*		  and whether there are any duplicates. 
-*		  Additionally, it handles specific cases such as the presence 
-*		  of negative zero ("-0") values.
+*		- After that, it checks if the input contains only numerical values. 
 */
 void	checking_arg(int argc, char **argv)
 {
@@ -87,7 +57,6 @@ void	checking_arg(int argc, char **argv)
 	{
 		ft_atoi(array[i]);
 		ft_is_num(array[i]);
-		ft_double_num(array);
 		i++;
 	}
 	if (argc == 2)
