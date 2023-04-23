2058f08e858541bf60533d45448d7b574fd211e5
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-04-23 19:39:55 +0200

    Update fill_stack.c

--- src/fill_stack.c
+++ src/fill_stack.c
@@ -6,11 +6,56 @@
 /*   By: jmetzger <jmetzger@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/15 14:39:00 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/04/23 14:31:32 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/04/23 19:15:25 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/* ft_free_array(): 
+*	Frees each element a 2D array (**array).
+*/
+char	**ft_free_array(char **s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != NULL)
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);
+	return (NULL);
+}
+
+/* ft_duplicates(): 
+*	Checks for duplicate numbers in the stack. 
+*		- Compares each node with the other nodes.
+*		- If the current node doesn't match with the other ones it 
+*		  moves to the next node.
+*/
+static void	ft_duplicates(t_stack *stack)
+{
+	t_stack	*current;
+	t_stack	*other_node;
+
+	current = stack;
+	while (current != NULL)
+	{
+		other_node = current->next;
+		while (other_node != NULL)
+		{
+			if (current->content == other_node->content)
+			{
+				ft_error("Error");
+			}
+			other_node = other_node->next;
+		}
+		current = current->next;
+	}
+	return ;
+}
 
 /* fill_stack(): 
 *	Fills stack_a with the provided values.
@@ -19,9 +64,11 @@
 *		- Next, the function creates a new node for each value in the array 
 *		  using the ft_lstnew_a() function and adds them to the back 
 *		  of the stack using the ft_lstadd_back_a() function.
-*		- Finally, the function checks the size of the stack. 
+*		- The function ft_duplicates() checks if the value inside each content
+*		  is more than once in the stack.
+*		- Finaly, The function checks the size of the stack. 
 *		  If the stack consists of only one node, the function exits 
-*		  the program with an error message.
+*		  the program.
 */
 void	fill_stack(t_data *data, int argc, char **argv)
 {
@@ -43,25 +90,9 @@ void	fill_stack(t_data *data, int argc, char **argv)
 		ft_lstadd_back_a(&data->stack_a, new);
 		i++;
 	}
+	ft_duplicates(data->stack_a);
 	if (ft_lstsize_a(data->stack_a) == 1)
 		exit(EXIT_FAILURE);
 	if (argc == 2)
 		ft_free_array(array);
-}
-
-/* ft_free_array(): 
-*	Frees each element a 2D array (**array).
-*/
-char	**ft_free_array(char **s)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (s[i] != NULL)
-	{
-		free(s[i]);
-		i++;
-	}
-	free(s);
-	return (NULL);
 }
